23f1f1b3d5a08accbd73e272e31d67e34dca6867
Guillaume Smet <guillaume.smet@gmail.com>
2023-04-05 15:50:54 +0200

    Upgrade to Quarkus 3.0.0.CR2

--- generated-platform-project/quarkus-maven-plugin/src/main/java/io/quarkus/maven/UpdateMojo.java
+++ generated-platform-project/quarkus-maven-plugin/src/main/java/io/quarkus/maven/UpdateMojo.java
@@ -65,7 +65,7 @@ public class UpdateMojo extends QuarkusProjectStateMojoBase {
     private Boolean rewriteDryRun;
 
     /**
-     * "The io.quarkus:quarkus-update-recipes version. This artifact contains the base recipes used by this tool to update a
+     * The io.quarkus:quarkus-update-recipes version. This artifact contains the base recipes used by this tool to update a
      * project.
      */
     @Parameter(property = "updateRecipesVersion", required = false)
@@ -77,7 +77,7 @@ public class UpdateMojo extends QuarkusProjectStateMojoBase {
     @Parameter(property = "stream", required = false)
     private String stream;
 
-    @Component
+    @Parameter(defaultValue = "${session}", readonly = true)
     private MavenSession mavenSession;
 
     @Component
